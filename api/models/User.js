c47a461ab634e6e50a13c642f3d93f9158573acc
developermartins <martssilvapro@gmail.com>
2024-03-04 20:15:23 -0300

    basic route & controller setup added

---
+++ api/models/User.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import db from '../config/connection.js';
+
+const User = db.define('user', {
+  username: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+});
+
+export default User;
