0d919385774c97a26b68f7f60732a9cfb63dc83f
developermartins <martssilvapro@gmail.com>
2024-03-04 19:02:44 -0300

    basic setup done

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import StatusCodes from 'http-status-codes';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+app.get('/', (req, res) => {
+  res.status(StatusCodes.OK).send('👌');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
