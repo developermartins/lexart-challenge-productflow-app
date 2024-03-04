a05abb06f23eed1283c2e2f101604090a64fdcb7
developermartins <martssilvapro@gmail.com>
2024-03-04 19:48:58 -0300

    sequelize & db connection setup done

--- api/index.js
+++ api/index.js
@@ -1,15 +1,33 @@
 import StatusCodes from 'http-status-codes';
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
 const app = express();
 const PORT = 8000;
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({
   extended: true,
 }));
+
+const db = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  }
+});
+
+db.authenticate()
+  .then(() => console.log('DB Connected...'))
+  .catch((err) => console.log('Error: ' +  err))
 
 app.get('/', (req, res) => {
   res.status(StatusCodes.OK).send('👌');
