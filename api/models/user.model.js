36aa0fd64f9d7bd03291a1efbeff5a050011e9e4
developermartins <martssilvapro@gmail.com>
2024-03-05 11:31:37 -0300

    user model added

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  });
+  (async () => {
+    await sequelize.sync();
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
