742ba18198e3efe12bb20d50a6f3da4487e18650
developermartins <martssilvapro@gmail.com>
2024-03-05 20:06:58 -0300

    authentication added

--- api/src/routes/product.route.js
+++ api/src/routes/product.route.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const product = require('../controllers/product.controller');
+const verifyToken = require('../middlewares/tokenMiddleware');
 
-router.get('/products', product.getAllProducts);
+router.get('/products', verifyToken, product.getAllProducts);
 
-router.get('/product/:id', product.getProductById);
+router.get('/product/:id', verifyToken, product.getProductById);
 
-router.get('/products/others', product.getProductByOthers);
+router.get('/products/others', verifyToken, product.getProductByOthers);
 
-router.post('/product/new', product.addProduct);
+router.post('/product/new', verifyToken, product.addProduct);
 
-router.put('/product/update/:id', product.updateProduct);
+router.put('/product/update/:id', verifyToken, product.updateProduct);
 
-router.delete('/product/delete/:id', product.deleteProduct);
+router.delete('/product/delete/:id', verifyToken, product.deleteProduct);
 
 module.exports = router;
