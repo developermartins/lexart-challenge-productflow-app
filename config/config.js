19242e6a172e27d68eb5e2a68dae0e5718f69b10
developermartins <martssilvapro@gmail.com>
2024-03-06 11:50:25 -0300

    dialectModule: pg added on config.js

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const db = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: process.env.POSTGRES_HOST,
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
