8f50d4440dbcabf6995f01bf6c881fbb4d0c1bf7
developermartins <martssilvapro@gmail.com>
2024-03-06 11:52:59 -0300

    import pg

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 require('dotenv').config();
 
 const db = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
